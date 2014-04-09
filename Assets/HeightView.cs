7a71e0f22b62e23c1525d670b07b94744a181d65
nobnak <nobu.nakata@gmail.com>
2014-04-10 02:08:25 +0900

    no message

---
+++ Assets/HeightView.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using FFTWSharp;
+using System.Runtime.InteropServices;
+
+public class HeightView : MonoBehaviour {
+	public int N;
+	public float length;
+	public Vector2 wind;
+	public float amplitude;
+	
+	private HeightSpectrum _spec;
+	private Texture2D _tex;
+	private float[] _height;
+
+	void Start () {
+		renderer.sharedMaterial.mainTexture = _tex = new Texture2D(N, N, TextureFormat.RGB24, false);
+		_tex.filterMode = FilterMode.Point;
+		_tex.anisoLevel = 0;
+
+		_spec = new HeightSpectrum(N, length, wind, amplitude);
+		_height = new float[2 * N * N];
+	}
+
+	void Update () {
+		var _fftIn = fftwf.malloc(8 * N * N);
+		var _fftOut = fftwf.malloc(8 * N * N);
+		try {
+			var plan = fftwf.dft_2d(N, N, _fftIn, _fftOut, fftw_direction.Backward, fftw_flags.Estimate);
+			var spectrum = _spec.Spectrum(Time.timeSinceLevelLoad);
+			var specShift = new float[spectrum.Length];
+			HeightSpectrum.ShiftComplex(spectrum, specShift, N);
+			spectrum = specShift;
+
+			Marshal.Copy(spectrum, 0, _fftIn, spectrum.Length);
+			fftwf.execute(plan);
+			Marshal.Copy(_fftOut, _height, 0, _height.Length);
+		} finally {
+			fftwf.free(_fftIn);
+			fftwf.free(_fftOut);
+		}
+
+		var amp = 1f / Mathf.Sqrt(N * N);
+		var colors = _tex.GetPixels();
+		for (var i = 0; i < colors.Length; i++) {
+			var h = _height[2 * i];
+			h = amp * 0.5f * (h + 1f);
+			colors[i] = new Color(h, h, h, 1f);
+		}
+		_tex.SetPixels(colors);
+		_tex.Apply();
+	}
+}
--- Assets/SpectrumView.cs
+++ Assets/SpectrumView.cs
@@ -1,72 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using FFTWSharp;
+using System.Runtime.InteropServices;
 
 public class SpectrumView : MonoBehaviour {
-}
+	public int N;
+	public float length;
+	public Vector2 wind;
+	public float amplitude;
 
-public class HeightSpectrum {
-	public const float TWO_PI = 2f * Mathf.PI;
-	public const float GRAVITY = 9.81f;
-	public const float w0 = TWO_PI / 200.0f;
+	private HeightSpectrum _spec;
+	private float[] _spectrumShift;
+	private Texture2D _tex;
 
-	private int _N;
-	private float _length;
-	private float _windSpeed;
-	private Vector2 _windDirection;
-	private float _amplitude;
+	void Start() {
+		_spec = new HeightSpectrum(N, length, wind, amplitude);
+		_spectrumShift = new float[2 * N * N];
 
-	private Vector2[] _heightSpectrum0;
-	private Vector2[] _heightSpectrum0Conj;
-	private float[] _omega;
+		renderer.sharedMaterial.mainTexture = _tex = new Texture2D(N, N, TextureFormat.RGB24, false);
+		_tex.filterMode = FilterMode.Point;
+		_tex.anisoLevel = 0;
+	}
 
-	public HeightSpectrum(int N, float length, Vector2 wind, float amplitude) {
-		this._N = N;
-		this._length = length;
-		this._windSpeed = wind.magnitude;
-		this._windDirection = wind.normalized;
-		this._amplitude = amplitude;
+	void Update() {
+		var spectrum = _spec.Spectrum(Time.timeSinceLevelLoad);
+		HeightSpectrum.ShiftComplex(spectrum, _spectrumShift, N);
+		var s = _spectrumShift;
 
-		this._heightSpectrum0 = new Vector2[N * N];
-		this._heightSpectrum0Conj = new Vector2[N * N];
-		this._omega = new float[N * N];
-
-		for (var m = 0; m < N; m++) {
-			for (var n = 0; n < N; n++) {
-				var index = m * N + n;
-				_heightSpectrum0[index] = StandardNormalDistribution() * Mathf.Sqrt(PhillipsSpectrum(n, m) * 0.5f);
-				_heightSpectrum0Conj[index] = StandardNormalDistribution() * Mathf.Sqrt(PhillipsSpectrum(N - n, N - m) * 0.5f);
-				_heightSpectrum0Conj[index].y *= -1f;
-				_omega[index] = Dispertion(n, m);
-			}
+		var colors = _tex.GetPixels();
+		for (var i = 0; i < colors.Length; i++) {
+			var sx = s[2 * i];
+			var sy = s[2 * i + 1];
+			colors[i] = new Color(Mathf.Abs(sx), Mathf.Abs(sy), 0f, 1f);
 		}
-	}
-
-	public static Vector2 StandardNormalDistribution() {
-		var u1 = Random.value;
-		var u2 = Random.value;
-		var sqrt = Mathf.Sqrt(-2f * Mathf.Log(u1));
-		var theta = TWO_PI * u2;
-		var z0 = sqrt * Mathf.Cos(theta);
-		var z1 = sqrt * Mathf.Sin(theta);
-		return new Vector2(z0, z1);
-	}
-
-	public float PhillipsSpectrum(int n, int m) {
-		var k = K(n, m);
-		var k2mag = k.sqrMagnitude;
-		var k6mag = k2mag * k2mag * k2mag;
-		var L = _windSpeed * _windSpeed / GRAVITY;
-		var kDotW = Vector2.Dot(k, _windDirection);
-		return _amplitude * Mathf.Exp(-1f / (k2mag * L * L)) * (kDotW * kDotW) / k6mag;
-	}
-
-	public float Dispertion(int n, int m) {
-		var k = K(n, m);
-		var w = Mathf.Sqrt(GRAVITY * k.magnitude);
-		return Mathf.Floor(w / w0) * w0;
-	}
-
-	public Vector2 K(int n, int m) {
-		return new Vector2(Mathf.PI * (2f * n - _N) / _length, Mathf.PI * (2f * m - _N) / _length);
+		_tex.SetPixels(colors);
+		_tex.Apply();
 	}
 }
+
